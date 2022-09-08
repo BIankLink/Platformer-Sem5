bc6755545713e71b71d34c527a4797cadd7bda34
BIankLink <Tanmaybosmiya11@gmail.com>
2022-09-08 16:01:35 +0530

    changedSomeUxAgain

--- Platformer/Assets/Scripts/SwipeInput.cs
+++ Platformer/Assets/Scripts/SwipeInput.cs
@@ -10,10 +10,11 @@ public class SwipeInput : MonoBehaviour
     public Vector2 startTouchPosition;
     public Vector2 currentTouchPosition;
     public int pixelDistToDetect = 50;
-    
+    [SerializeField]Joystick joystick;
     float tapRange = 10;
     public bool fingerDown;
     float screenWidth;
+    float ScreenWidthHalf;
     [SerializeField]public float moveDir;
     public bool up, down, left, right,moveLeft,moveRight,wallRunningInput=false;
     float timer=0f;
@@ -29,6 +30,7 @@ public class SwipeInput : MonoBehaviour
     void Start()
     {
         screenWidth = Screen.width;
+        ScreenWidthHalf = screenWidth / 2;
         player = GetComponent<PlayerStateMachine>();
     }
 
@@ -55,7 +57,7 @@ public class SwipeInput : MonoBehaviour
     public void handleInput()
     {
         Touch[] myTouches = Input.touches;
-
+        moveDir = joystick.Horizontal;
         if (switchTimer <=SwitchCoolDown)
         {
             
@@ -99,7 +101,7 @@ public class SwipeInput : MonoBehaviour
                 startTouchPosition = myTouches[i].position;
                 // Debug.Log("else "+startPos);
 
-
+                currentTouchPosition = myTouches[i].position;
 
                 fingerDown = true;
 
@@ -108,155 +110,68 @@ public class SwipeInput : MonoBehaviour
 
             if (i >= 0 && myTouches[i].phase == TouchPhase.Moved && i <= 2)
             {
-                currentTouchPosition = myTouches[i].position;
-                Vector2 direction = currentTouchPosition - startTouchPosition;
-                var angle = Vector2.Angle(Vector2.up,direction.normalized);
-                float distance = Vector2.Distance(startTouchPosition, currentTouchPosition);
-                if (fingerDown)
+               currentTouchPosition=myTouches[i].position;
+                if (currentTouchPosition.x > screenWidth / 2)
                 {
-                    if (distance > pixelDistToDetect)
-                    {
-                        if (direction.x > 0)
-                        {
-                            if (angle < 67.5)
-                            {
-                                Debug.Log("up");
-                                up = true; 
-                                if (player.CanSwitch)
-                                {
-                                    wallRunningInput = false;
-                                }
-                               
-                                StartCoroutine(uponce());
-                                fingerDown = false;
+                    CheckSwipe();
+                }
+            }
 
-                            }
-                            else if (angle < 150)
-                            {
-                                //right = true;
-                                //attacking = true;
-                                //if (!wallRunningInput)
-                                //{
-                                //    moveRight = false;
-                                //    moveLeft = false;
-                                //    moveDir = 0;
-                                //}
-                                //StartCoroutine(rightOnce());
-                                //fingerDown = false;
-                                //Debug.Log("right");
-                            }
-                            else if (angle < 180f)
-                            {
-                                Debug.Log("down");
-                                down = true;
-                                if (player.CanSwitch)
-                                {
-                                    wallRunningInput = true;
-                                }
-                                fingerDown = false;
+            //if (i >= 0 && myTouches[i].phase == TouchPhase.Stationary)
+            //{
+            //    if (!attacking)
+            //    {
+            //        if (timer >= startTimer)
+            //        {
+            //            if (fingerDown)
+            //            {
+            //                currentTouchPosition = myTouches[i].position;
+            //                Vector2 distance = currentTouchPosition - startTouchPosition;
+            //                if (Mathf.Abs(distance.x) < tapRange && Mathf.Abs(distance.y) < tapRange)
+            //                {
+            //                    if(currentTouchPosition.x < screenWidth / 2)
+            //                    {
+            //                        if (currentTouchPosition.x < ScreenWidthHalf/2)
+            //                        {
 
-                            }
+            //                            if (!moveRight)
+            //                            {
 
-                        }
-                        else
-                        {
-                            if (angle < 67.5)
-                            {
-                                Debug.Log("up");
-                                up = true;
-                                if (player.CanSwitch)
-                                {
-                                    wallRunningInput = false;
-                                }
+            //                                //Debug.Log("moveLeft");
+            //                                moveLeft = true;
+            //                                //fingerDown = true;
+            //                                if (moveDir >= -1)
+            //                                {
+            //                                    moveDir -= 0.1f;
+            //                                }
+
+            //                            }
+            //                        }
+            //                        else if (currentTouchPosition.x > ScreenWidthHalf / 2)
+            //                        {
+
+            //                            if (!moveLeft)
+            //                            {
+
+            //                                //Debug.Log("moveRight");
+            //                                moveRight = true;
+            //                                //fingerDown=true;
+            //                                if (moveDir <= 1)
+            //                                {
+            //                                    moveDir += 0.1f;
+            //                                }
+
+            //                            }
+            //                        }
+            //                    }
                                 
-                                StartCoroutine(uponce());
-                                fingerDown = false;
+            //                }
+            //            }
 
-                            }
-                            else if (angle < 150)
-                            {
-                                //left = true;
-                                //attacking = true;
-                                //if (!wallRunningInput)
-                                //{
-                                //    moveRight = false;
-                                //    moveLeft = false;
-                                //    moveDir = 0;
-                                //}
-                                //StartCoroutine(leftOnce());
-                                //fingerDown = false;
-                                ////Debug.Log("left");
-                            }
-                            else if (angle < 180f)
-                            {
-                                Debug.Log("down");
-                                down = true;
-                                if (player.CanSwitch)
-                                {
-                                    wallRunningInput = true;
-                                }
-                                fingerDown = false;
+            //        }
+            //    }
 
-                            }
-                            
-
-                        }
-                    }
-                }
-
-
-            }
-            if (i >= 0 && myTouches[i].phase == TouchPhase.Stationary)
-            {
-                if (!attacking)
-                {
-                    if (timer >= startTimer)
-                    {
-                        if (fingerDown)
-                        {
-                            currentTouchPosition = myTouches[i].position;
-                            Vector2 distance = currentTouchPosition - startTouchPosition;
-                            if (Mathf.Abs(distance.x) < tapRange && Mathf.Abs(distance.y) < tapRange)
-                            {
-                                if (currentTouchPosition.x < screenWidth / 2)
-                                {
-
-                                    if (!moveRight)
-                                    {
-
-                                        //Debug.Log("moveLeft");
-                                        moveLeft = true;
-                                        //fingerDown = true;
-                                        if (moveDir >= -1)
-                                        {
-                                            moveDir -= 0.1f;
-                                        }
-
-                                    }
-                                }
-                                else if (currentTouchPosition.x > screenWidth / 2)
-                                {
-
-                                    if (!moveLeft)
-                                    {
-
-                                        //Debug.Log("moveRight");
-                                        moveRight = true;
-                                        //fingerDown=true;
-                                        if (moveDir <= 1)
-                                        {
-                                            moveDir += 0.1f;
-                                        }
-
-                                    }
-                                }
-                            }
-                        }
-
-                    }
-                }
-
-            }
+           // }
             if (i >= 0 && myTouches[i].phase == TouchPhase.Ended)
             {
                 if (i == 0)
@@ -291,35 +206,35 @@ public class SwipeInput : MonoBehaviour
             if (tapCounter == 2)
             {
                 
-                   if(startTouchPosition.x < screenWidth / 2)
-                   {
-                        Debug.Log("right");
-                        left = true;
-                        attacking = true;
-                        if (!wallRunningInput)
-                        {
-                            moveRight = false;
-                            moveLeft = false;
-                            moveDir = 0;
-                        }
-                        StartCoroutine(leftOnce());
-                        fingerDown = false;
+                   //if(startTouchPosition.x < screenWidth / 2)
+                   //{
+                   //     Debug.Log("right");
+                   //     left = true;
+                   //     attacking = true;
+                   //     if (!wallRunningInput)
+                   //     {
+                   //         moveRight = false;
+                   //         moveLeft = false;
+                   //         moveDir = 0;
+                   //     }
+                   //     StartCoroutine(leftOnce());
+                   //     fingerDown = false;
 
-                   }
-                   else if(startTouchPosition.x > screenWidth / 2)
-                   {
-                        Debug.Log("right");
-                        right = true;
-                        attacking = true;
-                        if (!wallRunningInput)
-                        {
-                            moveRight = false;
-                            moveLeft = false;
-                            moveDir = 0;
-                        }
-                        StartCoroutine(rightOnce());
-                        fingerDown = false;
-                   }
+                   //}
+                   //else if(startTouchPosition.x > screenWidth / 2)
+                   //{
+                   //     Debug.Log("right");
+                   //     right = true;
+                   //     attacking = true;
+                   //     if (!wallRunningInput)
+                   //     {
+                   //         moveRight = false;
+                   //         moveLeft = false;
+                   //         moveDir = 0;
+                   //     }
+                   //     StartCoroutine(rightOnce());
+                   //     fingerDown = false;
+                   //}
 
 
                         //if (wallRunningInput)
@@ -348,6 +263,97 @@ public class SwipeInput : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         tapCounter = 0;
     }
+
+    bool SwipeDistanceCheckMet()
+    {
+        return VerticalMovementDistance() > pixelDistToDetect || HorizontalMovementDistance() > pixelDistToDetect;
+    }
+
+    float VerticalMovementDistance()
+    {
+        return Mathf.Abs(currentTouchPosition.y - startTouchPosition.y);
+    }
+    float HorizontalMovementDistance()
+    {
+        return Mathf.Abs(currentTouchPosition.x - startTouchPosition.x);
+    }
+
+    bool IsVerticalSwipe()
+    {
+        return VerticalMovementDistance() > HorizontalMovementDistance();
+    }
+
+    void CheckSwipe()
+    {
+        if (fingerDown)
+        {
+            if (SwipeDistanceCheckMet())
+            {
+                if (IsVerticalSwipe())
+                {
+
+
+                    if (currentTouchPosition.y - startTouchPosition.y > 0)
+                    {
+                        Debug.Log("up");
+                        up = true;
+                        if (player.CanSwitch)
+                        {
+                            wallRunningInput = false;
+                        }
+
+                        StartCoroutine(uponce());
+                        fingerDown = false;
+
+                    }
+                    else if (currentTouchPosition.y - startTouchPosition.y < 0)
+                    {
+                        Debug.Log("down");
+                        down = true;
+                        if (player.CanSwitch)
+                        {
+                            wallRunningInput = true;
+                        }
+                        fingerDown = false;
+                    }
+                }
+                else
+                {
+                    if (currentTouchPosition.x - startTouchPosition.x > 0)
+                    {
+                        right = true;
+                        attacking = true;
+                        if (!wallRunningInput)
+                        {
+                            moveRight = false;
+                            moveLeft = false;
+                            moveDir = 0;
+                        }
+                        StartCoroutine(rightOnce());
+                        fingerDown = false;
+                        //Debug.Log("right");
+                    }
+                    else if (currentTouchPosition.x - startTouchPosition.x < 0)
+                    {
+                        left = true;
+                        attacking = true;
+                        if (!wallRunningInput)
+                        {
+                            moveRight = false;
+                            moveLeft = false;
+                            moveDir = 0;
+                        }
+                        StartCoroutine(leftOnce());
+                        fingerDown = false;
+                        ////Debug.Log("left");
+                    }
+
+                }
+
+            }
+        }
+    }
+
 
     //void DashLeft()
     //{
