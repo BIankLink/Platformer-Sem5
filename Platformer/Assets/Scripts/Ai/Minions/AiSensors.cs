b15d85508baf63bb5e3ed11061ce830f6d7dc3f2
BIankLink <Tanmaybosmiya11@gmail.com>
2022-09-13 14:29:24 +0530

    added VisionCone

--- Platformer/Assets/Scripts/Ai/Minions/AiSensors.cs
+++ Platformer/Assets/Scripts/Ai/Minions/AiSensors.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class AiSensors : MonoBehaviour
 {
@@ -10,11 +11,19 @@ public class AiSensors : MonoBehaviour
     public Color meshColor=Color.yellow;
     [SerializeField]Vector3[] vertices;
     [SerializeField]int[] triangles;
-    Mesh mesh;
+    //Mesh mesh;
+    MeshFilter meshF;
+    [SerializeField]MeshCollider colider;
+    public Material material;
     // Start is called before the first frame update
     void Start()
     {
-        
+        meshF = GetComponent<MeshFilter>();
+        colider= meshF.gameObject.AddComponent<MeshCollider>();
+        meshF.mesh = CreateWedgeMesh();
+        //colider.sharedMesh = meshF.mesh;
+        //colider.isTrigger = true;
+        //colider.convex = true;
     }
 
     // Update is called once per frame
@@ -26,12 +35,13 @@ public class AiSensors : MonoBehaviour
     Mesh CreateWedgeMesh()
     {
         Mesh mesh = new Mesh();
-
-        int numTriagles = 8;
+        colider = new MeshCollider();
+        int segments = 10;
+        int numTriagles = (segments*4)+2+2;
         int numVertices = numTriagles * 3;
 
         vertices = new Vector3[numVertices];
-        triangles= new int[numTriagles];
+        triangles= new int[numVertices];
 
         Vector3 bottomCenter = Vector3.zero;
         Vector3 bottomLeft = Quaternion.Euler(0, -angle, 0) * Vector3.forward * distance;
@@ -61,24 +71,43 @@ public class AiSensors : MonoBehaviour
         vertices[vert++] = topRight;
         vertices[vert++] = bottomRight;
         vertices[vert++] = bottomCenter;
-        // far side
-        vertices[vert++] = bottomLeft;
-        vertices[vert++] = bottomRight;
-        vertices[vert++] = topRight;
 
-        vertices[vert++] = topRight;
-        vertices[vert++] = topLeft;
-        vertices[vert++] = bottomLeft;
-        //top side
-        vertices[vert++] = topCenter;
-        vertices[vert++] = topLeft;
-        vertices[vert++] = topRight;
-        //bottom
-        vertices[vert++] = bottomCenter;
-        vertices[vert++] = bottomRight;
-        vertices[vert++] = bottomLeft;
+        float currentAngle = -angle;
+        float deltaAngle = (angle * 2) / segments;
 
-        for(int i = 0; i < numTriagles; i++)
+        for(int i = 0; i < segments; i++)
+        {
+           
+            
+            bottomLeft = Quaternion.Euler(0, currentAngle, 0) * Vector3.forward * distance;
+            bottomRight = Quaternion.Euler(0, currentAngle+ deltaAngle, 0) * Vector3.forward * distance;
+           
+            
+            topLeft = bottomLeft + Vector3.up * height;
+            topRight = bottomRight + Vector3.up * height;
+
+            // far side
+            vertices[vert++] = bottomLeft;
+            vertices[vert++] = bottomRight;
+            vertices[vert++] = topRight;
+
+            vertices[vert++] = topRight;
+            vertices[vert++] = topLeft;
+            vertices[vert++] = bottomLeft;
+            //top side
+            vertices[vert++] = topCenter;
+            vertices[vert++] = topLeft;
+            vertices[vert++] = topRight;
+            //bottom
+            vertices[vert++] = bottomCenter;
+            vertices[vert++] = bottomRight;
+            vertices[vert++] = bottomLeft;
+
+
+            currentAngle += deltaAngle;
+        }
+              
+        for(int i = 0; i < numVertices; i++)
         {
             triangles[i] = i;
         }
@@ -86,20 +115,24 @@ public class AiSensors : MonoBehaviour
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
+        
+        GetComponent<MeshRenderer>().material = material;
+        meshF.mesh = mesh;
+        
         return mesh;
     }
 
     private void OnValidate()
     {
-        mesh=CreateWedgeMesh();
+        
     }
 
     private void OnDrawGizmos()
     {
-        if (mesh)
+        if (meshF)
         {
             Gizmos.color = meshColor;
-            Gizmos.DrawMesh(mesh,transform.position,transform.rotation);
+            Gizmos.DrawMesh(meshF.mesh,transform.position,transform.rotation);
         }
     }
 
--- Platformer/Assets/TheKiwiCoder/BehaviourTree/Scripts/Runtime/Context.cs
+++ Platformer/Assets/TheKiwiCoder/BehaviourTree/Scripts/Runtime/Context.cs
@@ -19,6 +19,7 @@ namespace TheKiwiCoder {
         public BoxCollider boxCollider;
         public CapsuleCollider capsuleCollider;
         public CharacterController characterController;
+        public AiSensors sensor;
         // Add other game specific systems here
 
         public static Context CreateFromGameObject(GameObject gameObject) {
@@ -33,6 +34,7 @@ namespace TheKiwiCoder {
             context.boxCollider = gameObject.GetComponent<BoxCollider>();
             context.capsuleCollider = gameObject.GetComponent<CapsuleCollider>();
             context.characterController = gameObject.GetComponent<CharacterController>();
+            context.sensor = gameObject.GetComponentInChildren<AiSensors>();
             
             // Add whatever else you need here...
 
